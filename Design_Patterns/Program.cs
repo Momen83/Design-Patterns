feeeb8d59a0b2648af6be76f767c9b429f3c2157
Momen83 <momenhatem2001@gmail.com>
2025-03-18 01:14:08 +0200

    Add Signleton Pattern

---
+++ Design_Patterns/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Design_Patterns.Singleton;
+
+Logger logger1, logger2;
+
+Task.Run(() => logger1 = Logger.getInstance());
+Task.Run(() => logger1 = Logger.getInstance());
+
+
+
+string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory , "logger.txt");
+Console.WriteLine(filePath);
+Logger.InfoMessage("Hello in Logger sytem\n", filePath);
+Logger.WarningMessage("Don't Touch Anything\n", filePath);
+Logger.ErrorMessage("Logger system terminated\n", filePath);
+
---
+++ Design_Patterns/Singleton/Logger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Singleton
+{
+    public class Logger
+    {
+        private static Logger instance;
+        private static readonly object _lock = new object();
+
+        private Logger()
+        {
+            
+        }
+
+        public static Logger getInstance()
+        {
+            lock (_lock)
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+
+                Console.WriteLine(instance.GetHashCode());
+
+                return instance;
+            }
+          
+        }
+
+        public static void InfoMessage(string message , string filePath)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+            File.AppendAllText(filePath, message);
+        }
+
+        public static void WarningMessage(string message , string filePath)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            File.AppendAllText(filePath, message);
+        }
+
+        public static void ErrorMessage(string message , string filePath)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            File.AppendAllText(filePath, message);
+        }
+    }
+}
